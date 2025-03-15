2804e40f3aef99202e407dca56778d9627edf0c8
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-03-14 19:20:46 -0500

    feat: Verificación del archivo ConexionDB.

---
+++ src/Conexion/ConexionDB.java
@@ -0,0 +1,9 @@
+package Conexion;
+
+import javax.swing.*;
+
+public class ConexionDB {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Aquí irá la conexión a la base de datos.");
+    }
+}
