9036909ea2ce6fccfe2ba44bd14e476cc10b6991
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-03-21 20:26:02 -0500

    feat: Conexión exitosa a la base de datos

--- src/Conexion/ConexionDB.java
+++ src/Conexion/ConexionDB.java
@@ -1,9 +1,25 @@
 package Conexion;
 
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexionDB {
+    public Connection getConnection(){
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/proyecto_ferreteria", "root", "");
+            System.out.println("Conexión exitosa a la base de datos.");
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión: " + e.getMessage());
+            JOptionPane.showMessageDialog(null, "Error en la conexión: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return con;
+    }
+
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Aquí irá la conexión a la base de datos.");
+        ConexionDB conexionDB = new ConexionDB();
+        conexionDB.getConnection();
     }
 }
