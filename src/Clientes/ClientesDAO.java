dd1d05b1ddefd01d9a63da11a160f706330ddeef
david satizabal <davidsatizabal705@gmail.com>
2025-03-23 13:42:36 -0500

    feat:se realizó la funcion agregar

---
+++ src/Clientes/ClientesDAO.java
@@ -0,0 +1,43 @@
+package Clientes;
+
+import Conexion.ConexionDB;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ClientesDAO {
+
+    private ConexionDB conexionDB = new ConexionDB();
+
+    public void agregar(Clientes clientes) {
+        Connection con = conexionDB.getConnection(); // Corregido
+
+        String query = "INSERT INTO clientes (nombre, telefono, direccion, correo) VALUES (?, ?, ?, ?)";
+
+        try
+        {
+            PreparedStatement pst = con.prepareStatement(query); // Corregido
+
+            pst.setString(1, clientes.getNombre());
+            pst.setString(2, clientes.getTelefono());
+            pst.setString(3, clientes.getDireccion()); // Corregido (antes faltaba)
+            pst.setString(4, clientes.getCorreo());
+
+            int resultado = pst.executeUpdate();
+            if (resultado > 0)
+            {
+                JOptionPane.showMessageDialog(null, "Cliente agregado exitosamente");
+            } else {
+                JOptionPane.showMessageDialog(null, "Cliente no agregado");
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+}
