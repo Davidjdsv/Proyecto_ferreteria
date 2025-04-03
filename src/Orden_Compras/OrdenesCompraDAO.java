5421e0b25cd6dbdc8a69fcc1a6b1349e912ab01f
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-04-02 23:59:45 -0500

    feat: Implementación del método agregar en OrdenesCompraDAO.java

---
+++ src/Orden_Compras/OrdenesCompraDAO.java
@@ -0,0 +1,34 @@
+package Orden_Compras;
+
+import Conexion.ConexionDB;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class OrdenesCompraDAO {
+    ConexionDB conexionDB = new ConexionDB();
+
+    public void agregar(OrdenesCompra ordenesCompra){
+        Connection con = conexionDB.getConnection();
+        String sql = "INSERT INTO ordenes_compra (id_cliente, id_empleado, id_producto, total, estado_orden, fecha_compra) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try{
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setInt(1, ordenesCompra.getId_cliente());
+            pst.setInt(2, ordenesCompra.getId_empleado());
+            pst.setInt(3, ordenesCompra.getId_producto());
+            pst.setInt(4, ordenesCompra.getTotal());
+            pst.setString(4, ordenesCompra.getEstado_orden());
+            pst.setString(4, ordenesCompra.getFecha_compra());
+
+            int resultado = pst.executeUpdate();
+            String mensaje = resultado > 0 ? "Orden de compra registrada con éxito!" : "Error al ingresar la orden de compra...";
+            int tipo_mensaje = resultado > 0 ? JOptionPane.INFORMATION_MESSAGE : JOptionPane.ERROR_MESSAGE;
+            JOptionPane.showMessageDialog(null, mensaje, "Ordenes compra:", tipo_mensaje);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
